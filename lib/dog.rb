5895c67059dbfd08bebdb7054cfcc02955eb0be2
michael otero <michaelotero1118@gmail.com>
2020-02-14 01:11:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -18,3 +18,25 @@ class Dog
   end
 end
 
+class Dog
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+end
